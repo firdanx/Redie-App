100762f49b1924e75810a562afcb835ef065a573
Flynn Kai <69429797+firdanx@users.noreply.github.com>
2023-08-02 01:57:54 +0700

    Update sidebar.php
    - Menu aktif ketika halaman sesuai

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -15,7 +15,7 @@
             <hr class="sidebar-divider my-0">
 
             <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
+            <li class = "<?=($title == 'Dashboard')?'nav-item active':'nav-item'?>">
                 <a class="nav-link" href="<?=base_url('dashboard');?>">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
@@ -44,7 +44,7 @@
                 </div>
             </li>
             <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
+            <li class = "<?=($title == 'Data Pasien Baru' || $title == 'Data Pasien Lama')?'nav-item active':'nav-item'?>">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapspasien"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-database"></i>
@@ -53,8 +53,8 @@
                 <div id="collapspasien" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Kelola Pasien:</h6>
-                        <a class="collapse-item" href="<?=base_url('pasien/pasien_baru');?>">Data Pasien Baru</a>
-                        <a class="collapse-item" href="<?=base_url('pasien/pasien_lama');?>">Data Pasien Lama</a>
+                        <a class="collapse-item <?=($title == 'Data Pasien Baru')?'active':''?>" href="<?=base_url('pasien/pasien_baru');?>">Data Pasien Baru</a>
+                        <a class="collapse-item <?=($title == 'Data Pasien Lama')?'active':''?>" href="<?=base_url('pasien/pasien_lama');?>">Data Pasien Lama</a>
                     </div>
                 </div>
             </li>
@@ -64,12 +64,13 @@
             <div class="sidebar-heading">
                Dokter & Poliklinik
             </div>
-            <li class="nav-item">
+            <li class = "<?=($title == 'Data Dokter')?'nav-item active':'nav-item'?>">
                 <a class="nav-link" href="<?=base_url('dokter')?>">
                     <i class="fas fa-fw fa-user-md"></i>
                     <span>Data Dokter</span></a>
             </li>
-            <li class="nav-item">
+            <li class = "<?=($title == 'Data Poliklinik')?'nav-item active':'nav-item'?>">
+            <!-- <li class="nav-item"> -->
                 <a class="nav-link" href="<?=base_url('poliklinik')?>">
                     <i class="fas fa-fw fa-clinic-medical"></i>
                     <span>Data Poliklinik</span></a>
@@ -80,7 +81,7 @@
             <div class="sidebar-heading">
                Laporan
             </div>
-            <li class="nav-item">
+            <li class = "<?=($title == 'Kunjungan')?'nav-item active':'nav-item'?>">
                 <a class="nav-link" href="index.html">
                     <i class="fas fa-fw fa-wheelchair"></i>
                     <span>Kunjungan</span></a>
